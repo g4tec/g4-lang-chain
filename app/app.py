bd9392bd3c3ca1e5121c96aa65d8127bde09d251
JailsonLucas <jailson@g4tech.com.br>
2024-08-16 17:39:00 -0300

    fix: adjust code after changes

--- app/app.py
+++ app/app.py
@@ -19,38 +19,7 @@ server_port = os.environ.get('SERVER_PORT')
 
 app.register_blueprint(lang_chain_bp, url_prefix='/lang_chain')
 
-@socketio.on('connect')
-def handle_connect():
-    socket_id = request.sid
-    print(f'Client connected with Socket ID: {socket_id}')
-    emit('connected', {'socket_id': socket_id})
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    print("Client disconnected")
-
-@socketio.on('prediction')
-def handle_stream(data):
-    question = data['question']
-    overrideConfig = data['overrideConfig']
-    weaviateIndex = overrideConfig['weaviateIndex']
-    sessionId = overrideConfig['sessionId']
-    weaviateHost = f"http://{overrideConfig['weaviateHost']}"
-    
-    docs = weaviate.get_docs(question, weaviateIndex, weaviateHost)
-
-    full_response = ''
-
-    for partial_response in lang_chain_chatbot.stream_bot_antropic(docs, question, sessionId):
-        emit('token', partial_response)
-        full_response += partial_response
-    sourceDocuments = []
-    for doc in docs:
-        sourceDocuments.append({
-                "pageContent": doc.page_content,
-                "metadata": doc.metadata,
-            })
-    emit('full_response',  {"text": full_response,"sourceDocuments":sourceDocuments})
 
 if __name__ == "__main__":
     import eventlet
@@ -60,6 +29,37 @@ if __name__ == "__main__":
     # Ensure eventlet is being used
     eventlet.monkey_patch()
     socketio = SocketIO(app, async_mode='eventlet')
+    @socketio.on('connect')
+    def handle_connect():
+        socket_id = request.sid
+        print(f'Client connected with Socket ID: {socket_id}')
+        emit('connected', {'socket_id': socket_id})
 
+    @socketio.on('disconnect')
+    def handle_disconnect():
+        print("Client disconnected")
+
+    @socketio.on('prediction')
+    def handle_stream(data):
+        question = data['question']
+        overrideConfig = data['overrideConfig']
+        weaviateIndex = overrideConfig['weaviateIndex']
+        sessionId = overrideConfig['sessionId']
+        weaviateHost = f"http://{overrideConfig['weaviateHost']}"
+        
+        docs = weaviate.get_docs(question, weaviateIndex, weaviateHost)
+
+        full_response = ''
+
+        for partial_response in lang_chain_chatbot.stream_bot_antropic(docs, question, sessionId):
+            emit('token', partial_response)
+            full_response += partial_response
+        sourceDocuments = []
+        for doc in docs:
+            sourceDocuments.append({
+                    "pageContent": doc.page_content,
+                    "metadata": doc.metadata,
+                })
+        emit('full_response',  {"text": full_response,"sourceDocuments":sourceDocuments})
     # Run the app with eventlet
     socketio.run(app, host="0.0.0.0", port=int(server_port), debug=True)
